557e1f3b5aaf932c04ee78d5904151e3aa42580f
SimonThrane <minijordan23-virum@hotmail.com>
2017-10-05 13:08:10 +0200

    Allow cors in order to use the android client

--- app.ts
+++ app.ts
@@ -23,8 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/programs' as any, programs as any);
-app.use('/api/exercises' as any, exercises as any);
+app.use('/programs' as any, programs as any);
+app.use('/exercises' as any, exercises as any);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,12 +34,12 @@ app.use(function(req, res, next) {
 
 // error handlers
 // Catch unauthorised errors
-app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
-  }
-});
+// app.use(function (err, req, res, next) {
+//   if (err.name === 'UnauthorizedError') {
+//     res.status(401);
+//     res.json({"message" : err.name + ": " + err.message});
+//   }
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
